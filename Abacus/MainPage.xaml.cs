1b92c6abbb3f223f791e799b13795dc087b7d523
James Bye <James_Bye@yahoo.co.uk>
2020-08-23 20:44:47 +0100

    Starting to work, displaying numbers

--- Abacus/MainPage.xaml.cs
+++ Abacus/MainPage.xaml.cs
@@ -40,62 +40,74 @@ namespace Abacus
             void ShowValueOnSoroban(long pValue)
             {
                 //declare variables
-
-                //This needs to be changed to toggle the elipse objects visibility
-                //Just start with a single digit number first, eg.7
-
+                string strMaxInput = pValue.ToString();
+                int[] arrNumsLarge = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
                 Ellipse H1, H2, E1, E2, E3, E4, E5;
-                H1 = (Ellipse)this.FindName("RH1C1");
-                H2 = (Ellipse)this.FindName("RH2C1");
-                E1 = (Ellipse)this.FindName("R1C1");
-                E2 = (Ellipse)this.FindName("R2C1");
-                E3 = (Ellipse)this.FindName("R3C1");
-                E4 = (Ellipse)this.FindName("R4C1");
-                E5 = (Ellipse)this.FindName("R5C1");
 
-                // Heaven beads
-                if (pValue >= 5)
+                // Put each digit of pValue into the arrNumsLarge array.
+                // The array represents each column of the soroban.
+                for (int i = 0; i < strMaxInput.Length; i++)
                 {
-                    H1.Visibility = Visibility.Collapsed;
-                    H2.Visibility = Visibility.Visible;
+                    arrNumsLarge[i + (12 - strMaxInput.Length)] = int.Parse(strMaxInput.Substring(i, 1));
                 }
-                else
+
+                //Loop through each column, right to left
+
+                for (int i = 0; i < strMaxInput.Length; i++)
                 {
-                    H1.Visibility = Visibility.Visible;
-                    H2.Visibility = Visibility.Collapsed;
-                }
-                // Earth beads
-                switch (pValue)
-                {
-                    case 0:
-                    case 5:
-                        E1.Visibility = E2.Visibility = E3.Visibility = E4.Visibility = Visibility.Visible;
-                        E5.Visibility = Visibility.Collapsed;
-                        break;
-                    case 1:
-                    case 6:
-                        E1.Visibility = E2.Visibility = E3.Visibility = E5.Visibility = Visibility.Visible;
-                        E4.Visibility = Visibility.Collapsed;
-                        break;
-                    case 2:
-                    case 7:
-                        E1.Visibility = E2.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
-                        E3.Visibility = Visibility.Collapsed;
-                        break;
-                    case 3:
-                    case 8:
-                        E1.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
-                        E2.Visibility = Visibility.Collapsed;
-                        break;
-                    case 4:
-                    case 9:
-                        E2.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
-                        E1.Visibility = Visibility.Collapsed;
-                        break;
-                    default:
-                        E1.Visibility = E2.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Collapsed;
-                        break;
-                }
+                    //H - Heaven bead, E - Earth bead
+                    H1 = (Ellipse)this.FindName("RH2C" + (12-i).ToString());
+                    H2 = (Ellipse)this.FindName("RH2C" + (12-i).ToString());
+                    E1 = (Ellipse)this.FindName("R1C"  + (12-i).ToString());
+                    E2 = (Ellipse)this.FindName("R2C"  + (12-i).ToString());
+                    E3 = (Ellipse)this.FindName("R3C"  + (12-i).ToString());
+                    E4 = (Ellipse)this.FindName("R4C"  + (12-i).ToString());
+                    E5 = (Ellipse)this.FindName("R5C"  + (12-i).ToString());
+
+                    // Heaven beads
+                    if (arrNumsLarge[i] >= 5)
+                    {
+                        H1.Visibility = Visibility.Collapsed;
+                        H2.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        H1.Visibility = Visibility.Visible;
+                        H2.Visibility = Visibility.Collapsed;
+                    }
+                    // Earth beads
+                    switch (arrNumsLarge[i])
+                    {
+                        case 0:
+                        case 5:
+                            E1.Visibility = E2.Visibility = E3.Visibility = E4.Visibility = Visibility.Visible;
+                            E5.Visibility = Visibility.Collapsed;
+                            break;
+                        case 1:
+                        case 6:
+                            E1.Visibility = E2.Visibility = E3.Visibility = E5.Visibility = Visibility.Visible;
+                            E4.Visibility = Visibility.Collapsed;
+                            break;
+                        case 2:
+                        case 7:
+                            E1.Visibility = E2.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
+                            E3.Visibility = Visibility.Collapsed;
+                            break;
+                        case 3:
+                        case 8:
+                            E1.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
+                            E2.Visibility = Visibility.Collapsed;
+                            break;
+                        case 4:
+                        case 9:
+                            E2.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
+                            E1.Visibility = Visibility.Collapsed;
+                            break;
+                        default:
+                            E1.Visibility = E2.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Collapsed;
+                            break;
+                    }
+                }//loop ends
             }
 
             //Gather inputs
