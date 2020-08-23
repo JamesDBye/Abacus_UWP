149c965a179924588315d94823845e8bdeeb7b1a
James Bye <James_Bye@yahoo.co.uk>
2020-08-23 18:49:49 +0100

    Programically refer to ellipse controls visibility

--- Abacus/MainPage.xaml.cs
+++ Abacus/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -42,70 +43,59 @@ namespace Abacus
 
                 //This needs to be changed to toggle the elipse objects visibility
                 //Just start with a single digit number first, eg.7
-                
+
+                Ellipse H1, H2, E1, E2, E3, E4, E5;
+                H1 = (Ellipse)this.FindName("RH1C1");
+                H2 = (Ellipse)this.FindName("RH2C1");
+                E1 = (Ellipse)this.FindName("R1C1");
+                E2 = (Ellipse)this.FindName("R2C1");
+                E3 = (Ellipse)this.FindName("R3C1");
+                E4 = (Ellipse)this.FindName("R4C1");
+                E5 = (Ellipse)this.FindName("R5C1");
+
                 // Heaven beads
                 if (pValue >= 5)
                 {
-                    RH2C1.Visibility = Visibility.Visible;
-                    RH1C1.Visibility = Visibility.Collapsed;
+                    H1.Visibility = Visibility.Collapsed;
+                    H2.Visibility = Visibility.Visible;
                 }
                 else
                 {
-                    RH2C1.Visibility = Visibility.Collapsed;
-                    RH1C1.Visibility = Visibility.Visible;
+                    H1.Visibility = Visibility.Visible;
+                    H2.Visibility = Visibility.Collapsed;
                 }
                 // Earth beads
                 switch (pValue)
                 {
                     case 0:
                     case 5:
-                        R1C1.Visibility = Visibility.Visible;
-                        R2C1.Visibility = Visibility.Visible;
-                        R3C1.Visibility = Visibility.Visible;
-                        R4C1.Visibility = Visibility.Visible;
-                        R5C1.Visibility = Visibility.Collapsed;
+                        E1.Visibility = E2.Visibility = E3.Visibility = E4.Visibility = Visibility.Visible;
+                        E5.Visibility = Visibility.Collapsed;
                         break;
                     case 1:
                     case 6:
-                        R1C1.Visibility = Visibility.Visible;
-                        R2C1.Visibility = Visibility.Visible;
-                        R3C1.Visibility = Visibility.Visible;
-                        R4C1.Visibility = Visibility.Collapsed;
-                        R5C1.Visibility = Visibility.Visible;
+                        E1.Visibility = E2.Visibility = E3.Visibility = E5.Visibility = Visibility.Visible;
+                        E4.Visibility = Visibility.Collapsed;
                         break;
                     case 2:
                     case 7:
-                        R1C1.Visibility = Visibility.Visible;
-                        R2C1.Visibility = Visibility.Visible;
-                        R3C1.Visibility = Visibility.Collapsed;
-                        R4C1.Visibility = Visibility.Visible;
-                        R5C1.Visibility = Visibility.Visible;
+                        E1.Visibility = E2.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
+                        E3.Visibility = Visibility.Collapsed;
                         break;
                     case 3:
                     case 8:
-                        R1C1.Visibility = Visibility.Visible;
-                        R2C1.Visibility = Visibility.Collapsed;
-                        R3C1.Visibility = Visibility.Visible;
-                        R4C1.Visibility = Visibility.Visible;
-                        R5C1.Visibility = Visibility.Visible;
+                        E1.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
+                        E2.Visibility = Visibility.Collapsed;
                         break;
                     case 4:
                     case 9:
-                        R1C1.Visibility = Visibility.Collapsed;
-                        R2C1.Visibility = Visibility.Visible;
-                        R3C1.Visibility = Visibility.Visible;
-                        R4C1.Visibility = Visibility.Visible;
-                        R5C1.Visibility = Visibility.Visible;
+                        E2.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Visible;
+                        E1.Visibility = Visibility.Collapsed;
                         break;
                     default:
-                        R1C1.Visibility = Visibility.Collapsed;
-                        R2C1.Visibility = Visibility.Collapsed;
-                        R3C1.Visibility = Visibility.Collapsed;
-                        R4C1.Visibility = Visibility.Collapsed;
-                        R5C1.Visibility = Visibility.Collapsed;
+                        E1.Visibility = E2.Visibility = E3.Visibility = E4.Visibility = E5.Visibility = Visibility.Collapsed;
                         break;
                 }
-
             }
 
             //Gather inputs
@@ -117,16 +107,6 @@ namespace Abacus
             DisplayTextBox.Text = Math.Max(longAdd1, longAdd2) + " is the larger number. We will display this first.";
             ShowValueOnSoroban(Math.Max(longAdd1, longAdd2));
 
-            /*
-            if (R1C1.Visibility == Visibility.Visible)
-            {
-                R1C1.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                R1C1.Visibility = Visibility.Visible;
-            }
-            */
         }
     }
 
