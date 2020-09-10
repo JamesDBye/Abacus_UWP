eb5f542befafdbbbb60c29c431083fb5a1156451
James Bye <James_Bye@yahoo.co.uk>
2020-09-10 19:38:09 +0100

    simplify introduction narrative

--- Abacus/Narratives.cs
+++ Abacus/Narratives.cs
@@ -39,7 +39,7 @@ namespace Abacus
             xstring += "\nThe top bead represents a &#39;5&#39;, and the four beads below represent a &#39;1&#39;. ";
             xstring += "To display &#39;7&#39; in the last column, you would need a &#39;5&#39; above plus &#39;2&#39; below. ";
             xstring += "&#39;Above&#39; and &#39;below&#39; are sometimes referred to as &#39;Heaven&#39; and &#39;Earth&#39;.\nAt the moment, the Soroban is displaying the number zero. To see what the number 25 would look like, ";
-            xstring += "type 25 into the #1 box and click the button labelled &#39;Display Number #1&#39; and it will display above. Alternatively, click one of the other buttons to see how to perform addition, subtraction or multiplication.";
+            xstring += "type 25 into the #1 box and click the button labelled &#39;Display Number #1&#39; and it will display above. Alternatively, click one of the other buttons to see how to count or perform addition.";
             xstring += " </More_info></Introduction><Addition><Comment1>";
             xstring += "When adding numbers on a stick, if the result is greater than 9 you must SUBTRACT a complementary number and add 1 to the stick on the left. [Complementary numbers: 1&amp;9, 2&amp;8, 3&amp;7, 4&amp;6, 5&amp;5, 6&amp;4, 7&amp;3, 8&amp;2, 9&amp;1 ]";
             xstring += "</Comment1><Comment2>";
